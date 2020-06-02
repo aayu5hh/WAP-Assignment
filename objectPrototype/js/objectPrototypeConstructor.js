cef0f435cf37c01d89e3ce8b132c9954ce0989f7
aayu5hh <me.aayus@gmail.com>
2020-06-02 08:47:36 -0500

    modules created

--- objectPrototype/js/objectPrototypeConstructor.js
+++ objectPrototype/js/objectPrototypeConstructor.js
@@ -1,23 +1,27 @@
 function Bike() {
 
 }
-Bike.prototype.speed = 0;
-Bike.prototype.applyBrake = function(value) {this.speed -= value;};
-Bike.prototype.speedup = function(value) {this.speed += value;};
 
-const bicycle = new Bike();
-console.log("Speeding up bicycle by 40..");
-bicycle.speedup(40);
-console.log("Applying brakes of 10..");
-bicycle.applyBrake(10);
-console.log(`So now the speed is ${bicycle.speed}!`);
+let bikeModule = (function(){
+  Bike.prototype.speed = 0;
+  Bike.prototype.applyBrake = function(value) {this.speed -= value;};
+  Bike.prototype.speedup = function(value) {this.speed += value;};
 
-const mountainBike = new Bike();
-Bike.prototype.gear = 1;
-Bike.prototype.setGear = function(gear) {this.gear = gear;};
+  const bicycle = new Bike();
+  console.log("Speeding up bicycle by 40..");
+  bicycle.speedup(40);
+  console.log("Applying brakes of 10..");
+  bicycle.applyBrake(10);
+  console.log(`So now the speed is ${bicycle.speed}!\n`);
 
-console.log(`The default speed of Mountain Bike is ${mountainBike.speed} & the gear is ${mountainBike.gear}`);
-mountainBike.speedup(20);
-mountainBike.applyBrake(3);
-mountainBike.setGear(3);
-console.log(`After setup, current speed of mountain bike is ${mountainBike.speed} & the gear is set to ${mountainBike.gear}`);
+  const mountainBike = new Bike();
+  Bike.prototype.gear = 1;
+  Bike.prototype.setGear = function(gear) {this.gear = gear;};
+
+  console.log(`The default speed of Mountain Bike is ${mountainBike.speed} & the gear is ${mountainBike.gear}`);
+  mountainBike.speedup(20);
+  mountainBike.applyBrake(3);
+  mountainBike.setGear(3);
+  console.log(`After setup, current speed of mountain bike is ${mountainBike.speed} & the gear is set to ${mountainBike.gear}`);
+
+})();
