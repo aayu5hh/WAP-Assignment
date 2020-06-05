a0237bf90ed26a801a01db36cd218ca583dd0af1
aayu5hh <me.aayus@gmail.com>
2020-06-05 16:06:04 -0500

    ajax assignment final

--- ajax/ex2/user.js
+++ ajax/ex2/user.js
@@ -4,6 +4,7 @@ $(function() {
   base_url = "https://jsonplaceholder.typicode.com";
   $("#submit").click(showDetails);
   $("#show-post-btn").click(showAllPost);
+  $("#all-post").on("click", "div #myCmt", showComments);
 });
 
 function showDetails() {
@@ -34,18 +35,46 @@ function showAllPost() {
     .then(myJson => parsePostJson(myJson, id))
     .catch(err => alert(err));
 
+  $("#show-post-btn").hide();
+
 }
 
 function parsePostJson(myJson, id) {
+  $("#all-post").append(
+      $("<h2>").text(`All Posts of ${$("#userName").text()}`).css("text-decoration", "underline")
+  );
+
   for(let i=0; i<myJson.length; i++){
 
     if(myJson[i].userId == id){
-      $("#all-post").append($("<p>").addClass("postTitle").text(myJson[i].title))
+      $("#all-post")
+      .append($("<p>").addClass("postTitle").text(myJson[i].title))
       .append($("<q>").text(myJson[i].body))
-      .append($("<button>").attr("value", `${id}`).attr("class", "myCmt").text("Comments"));
-
+      .append($("<button>").addClass(`${myJson[i].id}-cmt`).attr("id", `myCmt`).text("Comments"))
+      .append($("<hr>"));
     }
 
   }
   $("#all-post").show();
+}
+
+function showComments() {
+  let postid = parseInt($(this).attr("class"));
+  fetch(`${base_url}/comments?postid=${postid}`)
+    .then(response => response.json())
+    .then(myJson => {
+      for(let i=0; i<myJson.length; i++){
+
+        if(myJson[i].postId==postid) {
+          $("<div>")
+          .addClass("commentDiv")
+          .text(myJson[i].body)
+          .insertAfter(`[class=${$(this).attr("class")}]`);
+        }
+
+      }
+    })
+    .catch(err => alert(err));
+    $(`[class=${$(this).attr("class")}]`).hide();
+
 }
